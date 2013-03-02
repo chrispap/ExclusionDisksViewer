f4f44ddc30a32666efe4055b78e89cd507663228
Chris Papapavlou (ubuntu 12.04) <chrispapapaulou@gmail.com>
2013-03-03 01:24:36 +0200

    Fixed Indentation

--- src/Disk.java
+++ src/Disk.java
@@ -11,26 +11,25 @@ public class Disk {
     boolean max = false;
 
     public Disk(int x, int y, int width, int height) {
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
 
-	this.r = (width + height) / 2;
+        this.r = (width + height) / 2;
     }
 
     public Disk(int x, int y, double r) {
-	this.x = x;
-	this.y = y;
-	this.r = r;
+        this.x = x;
+        this.y = y;
+        this.r = r;
 
-	this.width = (int) (2 * r);
-	this.height = (int) (2 * r);
+        this.width = (int) (2 * r);
+        this.height = (int) (2 * r);
     }
 
     public Arc2D.Double getArc() {
-	return new Arc2D.Double(x - width / 2, y - width / 2, width, height, 0,
-		360, Arc2D.Double.CHORD);
+        return new Arc2D.Double(x - width / 2, y - width / 2, width, height, 0, 360, Arc2D.Double.CHORD);
     }
 
 }
--- src/DiskViewer.java
+++ src/DiskViewer.java
@@ -39,12 +39,9 @@ public class DiskViewer extends JPanel {
     static final String user = System.getProperty("user.name");
     static final String dirPath = System.getProperty("user.dir") + "/";
     static final String imgPath = dirPath + "plot.png";
-    static final String[] diskPaths = { dirPath + "disk1.txt",
-	    dirPath + "disk2.txt", };
-    static final Color[] fillCol = { new Color(255, 255, 255, 80),
-	    new Color(0, 255, 055, 80), };
-    static final String[] algorithms = { "General IE Algorithm",
-	    "Our New Approach", };
+    static final String[] diskPaths = { dirPath + "disk1.txt", dirPath + "disk2.txt", };
+    static final Color[] fillCol = { new Color(255, 255, 255, 80), new Color(0, 255, 055, 80), };
+    static final String[] algorithms = { "General IE Algorithm", "Our New Approach", };
     static final Color borderCol = new Color(255, 0, 0, 0);
 
     /* Instance Variables */
@@ -62,24 +59,23 @@ public class DiskViewer extends JPanel {
      *            disks
      * @throws IOException
      */
-    public DiskViewer(String plotImgPath, String[] diskDataPaths)
-	    throws IOException {
-	plotImg = ImageIO.read(new File(plotImgPath));
-	setPreferredSize(new Dimension(plotImg.getWidth(), plotImg.getHeight()));
-	setMinimumSize(new Dimension(600, 400));
-	setBackground(Color.BLACK);
+    public DiskViewer(String plotImgPath, String[] diskDataPaths) throws IOException {
+        plotImg = ImageIO.read(new File(plotImgPath));
+        setPreferredSize(new Dimension(plotImg.getWidth(), plotImg.getHeight()));
+        setMinimumSize(new Dimension(600, 400));
+        setBackground(Color.BLACK);
 
-	diskFullList = new ArrayList<Disk>(9000);
-	diskPlotList = new ArrayList<Disk>(20);
+        diskFullList = new ArrayList<Disk>(9000);
+        diskPlotList = new ArrayList<Disk>(20);
 
-	for (int i = 0; i < diskDataPaths.length; i++)
-	    loadDiskData(diskDataPaths[i], i);
+        for (int i = 0; i < diskDataPaths.length; i++)
+            loadDiskData(diskDataPaths[i], i);
 
-	addMouseListener(new MouseAdapter() {
-	    public void mousePressed(MouseEvent evt) {
-		showDisk(evt.getPoint(), activeGroup);
-	    }
-	});
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent evt) {
+                showDisk(evt.getPoint(), activeGroup);
+            }
+        });
     }
 
     /**
@@ -87,21 +83,20 @@ public class DiskViewer extends JPanel {
      * Use different color for disks of different groups.
      */
     public void paint(Graphics g) {
-	super.paint(g);
-	Graphics2D gg = (Graphics2D) g;
-	gg.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-		RenderingHints.VALUE_ANTIALIAS_ON);
+        super.paint(g);
+        Graphics2D gg = (Graphics2D) g;
+        gg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-	/* Draw the plot image */
-	gg.drawImage(plotImg, null, 0, 0);
+        /* Draw the plot image */
+        gg.drawImage(plotImg, null, 0, 0);
 
-	/* Draw the disks */
-	for (Disk d : diskPlotList) {
-	    gg.setColor(fillCol[d.group]);
-	    gg.fill(d.getArc());
-	    gg.setColor(borderCol);
-	    gg.draw(d.getArc());
-	}
+        /* Draw the disks */
+        for (Disk d : diskPlotList) {
+            gg.setColor(fillCol[d.group]);
+            gg.fill(d.getArc());
+            gg.setColor(borderCol);
+            gg.draw(d.getArc());
+        }
 
     }
 
@@ -114,40 +109,37 @@ public class DiskViewer extends JPanel {
      *            The group that the loaded disks will belong to.
      * @throws IOException
      */
-    protected void loadDiskData(String diskDataPath, int grp)
-	    throws IOException {
+    protected void loadDiskData(String diskDataPath, int grp) throws IOException {
 
-	/* Populate the disk list */
-	FileInputStream fstream = new FileInputStream(diskDataPath);
-	DataInputStream in = new DataInputStream(fstream);
-	BufferedReader br = new BufferedReader(new InputStreamReader(in));
-	String line, p[];
-	Disk d1, d2;
+        /* Populate the disk list */
+        FileInputStream fstream = new FileInputStream(diskDataPath);
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line, p[];
+        Disk d1, d2;
 
-	Integer.parseInt(br.readLine()); // skip first line
-	line = br.readLine();
-	p = line.split(",");
-	d1 = new Disk(Integer.parseInt(p[0]), 1000 - Integer.parseInt(p[1]),
-		Integer.parseInt(p[2]), Integer.parseInt(p[3]));
+        Integer.parseInt(br.readLine()); // skip first line
+        line = br.readLine();
+        p = line.split(",");
+        d1 = new Disk(Integer.parseInt(p[0]), 1000 - Integer.parseInt(p[1]), Integer.parseInt(p[2]),
+                Integer.parseInt(p[3]));
 
-	while ((line = br.readLine()) != null) {
-	    p = line.split(",");
-	    d2 = new Disk(Integer.parseInt(p[0]),
-		    1000 - Integer.parseInt(p[1]), Integer.parseInt(p[2]),
-		    Integer.parseInt(p[3]));
+        while ((line = br.readLine()) != null) {
+            p = line.split(",");
+            d2 = new Disk(Integer.parseInt(p[0]), 1000 - Integer.parseInt(p[1]), Integer.parseInt(p[2]),
+                    Integer.parseInt(p[3]));
 
-	    if (!(d2.x == d1.x && d2.y == d1.y))
-		d1.max = true;
+            if (!(d2.x == d1.x && d2.y == d1.y)) d1.max = true;
 
-	    d1.group = grp;
-	    diskFullList.add(d1);
-	    d1 = d2;
+            d1.group = grp;
+            diskFullList.add(d1);
+            d1 = d2;
 
-	}
+        }
 
-	in.close();
+        in.close();
 
-	diskPlotList = new ArrayList<Disk>(20);
+        diskPlotList = new ArrayList<Disk>(20);
     }
 
     /**
@@ -157,7 +149,7 @@ public class DiskViewer extends JPanel {
      *            The new active group.
      */
     public void setActiveGroup(int grp) {
-	activeGroup = grp;
+        activeGroup = grp;
     }
 
     /**
@@ -169,38 +161,36 @@ public class DiskViewer extends JPanel {
      *            The group that we are interested.
      */
     protected void showDisk(Point p, int grp) {
-	ArrayList<Disk> rm = new ArrayList<Disk>(20);
+        ArrayList<Disk> rm = new ArrayList<Disk>(20);
 
-	for (Disk d : diskFullList)
-	    if (d.group == grp && !(hideSameCenteredDisks && !d.max)
-		    && d.getArc().contains(p))
-		rm.add(d);
+        for (Disk d : diskFullList)
+            if (d.group == grp && !(hideSameCenteredDisks && !d.max) && d.getArc().contains(p)) rm.add(d);
 
-	for (Disk d : rm) {
-	    diskPlotList.add(d);
-	    diskFullList.remove(d);
-	}
+        for (Disk d : rm) {
+            diskPlotList.add(d);
+            diskFullList.remove(d);
+        }
 
-	repaint();
+        repaint();
     }
 
     /**
      * Show AAAALL the disks that are loaded from all files
      */
     public void showAllDisks() {
-	hideAllDisks();
-	diskPlotList = diskFullList;
-	diskFullList = new ArrayList<Disk>(1);
-	repaint();
+        hideAllDisks();
+        diskPlotList = diskFullList;
+        diskFullList = new ArrayList<Disk>(1);
+        repaint();
     }
 
     /**
      * Hide all the disks that are currently visible.
      */
     public void hideAllDisks() {
-	diskFullList.addAll(diskPlotList);
-	diskPlotList.clear();
-	repaint();
+        diskFullList.addAll(diskPlotList);
+        diskPlotList.clear();
+        repaint();
     }
 
     /**
@@ -209,58 +199,58 @@ public class DiskViewer extends JPanel {
      */
     public static void main(String[] args) {
 
-	try {
-	    /* Create GUI components */
-	    final JFrame win = new JFrame("Exclusion Disks Viewer");
-	    final DiskViewer plotPanel = new DiskViewer(imgPath, diskPaths);
-	    final JPanel buttonPanel = new JPanel();
-	    final JButton hideButton = new JButton("Hide All");
-	    final JButton showButton0 = new JButton(algorithms[0]);
-	    final JButton showButton1 = new JButton(algorithms[1]);
+        try {
+            /* Create GUI components */
+            final JFrame win = new JFrame("Exclusion Disks Viewer");
+            final DiskViewer plotPanel = new DiskViewer(imgPath, diskPaths);
+            final JPanel buttonPanel = new JPanel();
+            final JButton hideButton = new JButton("Hide All");
+            final JButton showButton0 = new JButton(algorithms[0]);
+            final JButton showButton1 = new JButton(algorithms[1]);
 
-	    /* Assign operations to the buttons */
-	    showButton0.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    plotPanel.setActiveGroup(0);
-		    showButton0.setEnabled(false);
-		    showButton1.setEnabled(true);
-		}
-	    });
-	    showButton1.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    plotPanel.setActiveGroup(1);
-		    showButton0.setEnabled(true);
-		    showButton1.setEnabled(false);
-		}
-	    });
-	    hideButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    plotPanel.hideAllDisks();
-		}
-	    });
+            /* Assign operations to the buttons */
+            showButton0.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    plotPanel.setActiveGroup(0);
+                    showButton0.setEnabled(false);
+                    showButton1.setEnabled(true);
+                }
+            });
+            showButton1.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    plotPanel.setActiveGroup(1);
+                    showButton0.setEnabled(true);
+                    showButton1.setEnabled(false);
+                }
+            });
+            hideButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    plotPanel.hideAllDisks();
+                }
+            });
 
-	    /* Initialize */
-	    plotPanel.activeGroup = 0;
-	    showButton0.setEnabled(false);
-	    showButton1.setEnabled(true);
+            /* Initialize */
+            plotPanel.activeGroup = 0;
+            showButton0.setEnabled(false);
+            showButton1.setEnabled(true);
 
-	    /* Add the components to the window,panels - Show window */
-	    buttonPanel.setLayout(new GridLayout(1, 0));
-	    buttonPanel.add(showButton0);
-	    buttonPanel.add(showButton1);
-	    buttonPanel.add(hideButton);
+            /* Add the components to the window,panels - Show window */
+            buttonPanel.setLayout(new GridLayout(1, 0));
+            buttonPanel.add(showButton0);
+            buttonPanel.add(showButton1);
+            buttonPanel.add(hideButton);
 
-	    win.add(plotPanel, BorderLayout.CENTER);
-	    win.add(buttonPanel, BorderLayout.NORTH);
-	    win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    win.setBackground(Color.BLACK);
-	    win.setExtendedState(win.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+            win.add(plotPanel, BorderLayout.CENTER);
+            win.add(buttonPanel, BorderLayout.NORTH);
+            win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            win.setBackground(Color.BLACK);
+            win.setExtendedState(win.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 
-	    win.setVisible(true);
+            win.setVisible(true);
 
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
